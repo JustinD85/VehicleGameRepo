e3629a43f9b5627f0de1294fd1f72e284bf16b78
Justin Duncan <falsemotive@icloud.com>
2017-10-26 13:51:20 +0900

    Created Initial map from World Machine parameters. 2x2K map, hilly.

---
+++ VehicleGame/Source/VehicleGame/VehicleGameGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "VehicleGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VEHICLEGAME_API AVehicleGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
